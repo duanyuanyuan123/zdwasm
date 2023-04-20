538a92e306bd49bf62b7d078f0d43e0f87797c79
“duanyuanyuan123” <2469241792@qq.com>
2023-04-20 10:54:54 +0800

    deco

--- build_decoder.sh
+++ build_decoder.sh
@@ -16,17 +16,16 @@ mkdir -p dist/simd
 #echo "make install"
 #make install
 
-cd ../ffmpeg
-echo "emconfigure"
+echo "emconfigure with simd"
 emconfigure ./configure --cc="emcc" --cxx="em++" --ar="emar" --ranlib="emranlib" --prefix=$(pwd)/../zdwasm/dist/nosimd --enable-cross-compile --target-os=none \
         --arch=x86_32 --cpu=generic --disable-gpl --enable-version3 --disable-avdevice --disable-swresample --disable-postproc --disable-avfilter \
         --disable-programs --disable-logging --disable-everything --enable-avformat --enable-decoder=hevc --enable-decoder=h264 \
         --enable-encoder=h264_qsv --enable-decoder=h264_qsv \
-	--enable-decoder=aac --disable-ffplay --disable-ffprobe --disable-asm --disable-doc --disable-devices --disable-network \
+        --enable-decoder=aac --disable-ffplay --disable-ffprobe --disable-asm --disable-doc --disable-devices --disable-network \
         --disable-parsers --disable-bsfs --disable-debug --enable-protocol=file --enable-demuxer=mov --enable-demuxer=flv --enable-demuxer=aac --enable-parser=aac \
           --enable-nonfree --disable-indevs --disable-outdevs \
-	  --extra-cflags="-Wno-unknown-warning-option -mfloat-abi=softfp -mfpu=neon" \
-	 --extra-ldflags="-L/opt/intel/mediasdk/lib -s PROXY_TO_PTHREAD=1 -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=4" 
+          --extra-cflags="-Wno-unknown-warning-option -msimd128 -msse -mfloat-abi=softfp -mfpu=neon" \
+         --extra-ldflags="-L/opt/intel/mediasdk/lib -s PROXY_TO_PTHREAD=1 -s USE_PTHREADS=1 -s PTHREAD_POOL_SIZE=4"
 if [ -f "Makefile" ]; then
   echo "make clean"
   make clean
